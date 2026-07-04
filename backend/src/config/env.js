690076113649315b64e6338a2c6b40906672f237
Aniket Raut <rautaniket857@gmail.com>
2026-07-04 16:59:40 +0530

    Initial commit

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -34,7 +34,7 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 5000;
 
 export const CLIENT_URL =
-  process.env.CLIENT_URL || 'http://localhost:5173';
+  process.env.CLIENT_URL || 'ai-fitness-planner-auth-based.vercel.app';
 
 export const MONGO_URI = process.env.MONGO_URI;
 
